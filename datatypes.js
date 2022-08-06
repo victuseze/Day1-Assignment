94b841ef98a04773fd7d5ee3b895867a043a7aa1
victuseze <106381679+victuseze@users.noreply.github.com>
2022-08-06 23:17:21 +0100

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,33 @@
+//Question 5
+console.log(typeof 'Victus');
+console.log(typeof 100)
+console.log(typeof true)
+console.log(typeof undefined)
+console.log(typeof null)
+
+// Question 6
+let name
+let ages
+let state
+let countries
+
+// Question 7
+let newName = 'Victus';
+let newAge = 100;
+let newState = 'Enugu';
+let newCountry = 'United Kingdom';
+
+//Question 8
+let firstName = 'Victus';
+let lastName = 'Eze';
+let isMarried = false;
+let country = 'United Kingdom';
+let age = 200
+
+// Quesstion 9
+let newFirstName = 'Victus', newLastName = 'Eze', newIsMarried = false, newCountries = 'United Kingdom', newAges = 100 
+
+// Question 10
+let  myAge = 100;
+let yourAge = 200;
+console.log(myAge, yourAge)
---
+++ variable.js
@@ -0,0 +1,6 @@
+// Question 4
+let name = 'Victus';
+let age = 100;
+let isMale = true;
+let state
+let country =
